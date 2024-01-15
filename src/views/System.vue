df79ab9a728a8ce092ac12478f644f314481407b
bascio <hjhjg55sajda@interia.pl>
2024-01-15 23:13:08 +0100

    Migrate files from previous project

---
+++ src/views/System.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="content" class="main">
+    <div class="container">
+      <div class="content-header row">
+        <div class="btn btn-link">
+          <a href="{% url 'tools:index' %}">bascio<span class="green-text">.dev</span></a>
+        </div>
+        <span class="content-header-title"></span>
+        {% if user.is_authenticated %}
+        <div>
+          <a href="{% url 'logout' %}" class="btn btn-link" data-toggle="tooltip" title="Log out">
+            <span class="material-icons-two-tone">logout</span>
+          </a>
+          <a href="{ url 'account' }" class="btn btn-primary">
+            <span class="material-icons-two-tone">account_circle</span> Account
+          </a>
+        </div>
+        {% else %}
+        <a href="{% url 'login' %}" class="btn btn-primary ">
+          <span class="material-icons-two-tone">login</span> Log in
+        </a>
+        {% endif %}
+      </div>
+      <div class="content">
+        <Tools/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Tools from './Tools.vue'
+
+</script>
+
+<style scoped lang="scss">
+  .container {
+    margin-top: 5vh;
+    min-height: 85vh;
+    background-color: var(--color-main-background);
+    border-radius: var(--default-border-radius);
+    -webkit-box-shadow: 0 6px 20px 0 var(--color-box-shadow);
+    -moz-box-shadow: 0 6px 20px 0 var(--color-box-shadow);
+    box-shadow: 0 6px 20px 0 var(--color-box-shadow);
+  }
+
+  .content-header {
+    border-bottom-color: var(--color-border);
+    border-bottom-style: dashed;
+    padding: 10px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: space-between;
+    align-items: center
+  }
+
+  .content-header-title {
+    font-weight: bold;
+    font-size: calc(var(--default-font-size) * 1.5);
+  }
+
+  .content {
+    padding: 15px;
+  }
+</style>
---
+++ src/views/Tools.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="cards-wrapper row">
+      <div class="card-wrapper col-md-6 col-lg-4 align-center">
+        <div class="card btn-card" data-color="" data-url="{% url module.url %}">
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <h6 class="card-subtitle mb-2"><span class="badge bg-primary"></span></h6>
+            <p class="card-text"></p>
+          </div>
+        </div>
+      </div>
+      {% endfor %}
+    </div>
+  </div>
+  {% endblock %}
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+  .card-wrapper {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+  }
+
+  .card {
+    width: 18rem;
+  }
+
+  .btn-card {
+    transition-timing-function: ease-out;
+    transition-duration: 250ms;
+    -webkit-transition-timing-function: ease-out;
+    -webkit-transition-duration: 250ms;
+    -moz-transition-timing-function: ease-out;
+    -moz-transition-duration: 250ms;
+  }
+
+  .btn-card:hover {
+    opacity: .8;
+    transform: scale(1.04);
+    cursor: pointer;
+  }
+</style>
