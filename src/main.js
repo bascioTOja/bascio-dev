14157205e0ab7dd281f8beebb9ad99f9b387eb0a
bascio <hjhjg55sajda@interia.pl>
2024-09-29 17:57:24 +0200

    Initial form for interest calculator

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue';
 import axios from 'axios';
 import { createPinia } from 'pinia';
 import router from '@/router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fas);
+library.add(fab);
+library.add(far);
 
 axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL;
 
@@ -14,6 +23,7 @@ if (accessToken) {
 const pinia = createPinia();
 const app = createApp(App);
 
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(pinia);
 app.use(router);
 
