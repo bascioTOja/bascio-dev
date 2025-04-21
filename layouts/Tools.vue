d458a7d648999c3df8c77a76c763b4dccb2d6f7e
bascio <hjhjg55sajda@interia.pl>
2025-04-21 12:56:17 +0200

    Add Tools layout

---
+++ layouts/Tools.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container mx-auto mt-[5vh] min-h-[85vh] bg-(--ui-bg) rounded-(--default-border-radius) pr-[.75rem] pl-[.75rem] shadow-default">
+    <div class="content-header border-b border-dashed border-(--ui-border) p-3 flex flex-wrap justify-between items-center">
+      <!-- Logo -->
+      <div class="btn-link">
+        <NuxtLink to="/" class="font-bold hover:opacity-90 transition-opacity duration-200">
+          bascio<span class="text-green-400">.dev</span>
+        </NuxtLink>
+      </div>
+
+      <span class="content-header-title">
+<!--          <UBreadcrumb class="text-lg">-->
+<!--            <UBreadcrumbItem to="/tools">tools</UBreadcrumbItem>-->
+<!--            <UBreadcrumbItem v-if="selectedCard">-->
+<!--              {{ selectedCard }}-->
+<!--            </UBreadcrumbItem>-->
+<!--          </UBreadcrumb>-->
+      </span>
+
+      <div class="flex flex-col">
+        <USkeleton v-if="isLoading" class="h-9 w-[136px]" />
+        <div v-else-if="status === 'authenticated'" class="flex items-center">
+          <UButton
+            color="gray"
+            variant="ghost"
+            class="mr-2"
+            @click="logOutHandler"
+          >
+            logout
+          </UButton>
+          <NuxtLink to="/profile">
+            <UButton color="green" class="font-bold">Account</UButton>
+          </NuxtLink>
+        </div>
+        <div v-else>
+          <NuxtLink to="/auth/login">
+            <UButton color="green" class="font-bold">Log in</UButton>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+
+    <div class="content p-4 flex flex-wrap md:justify-start justify-center items-stretch">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { signOut, status } = useAuth();
+const isLoading = computed(() => status.value === 'loading');
+
+const logOutHandler = async () => {
+  await signOut({ callbackUrl: '/' });
+};
+</script>
+
+<style scoped lang="scss">
+.shadow-default {
+  -webkit-box-shadow: 0 6px 20px 0 var(--color-box-shadow);
+  -moz-box-shadow: 0 6px 20px 0 var(--color-box-shadow);
+  box-shadow: 0 6px 20px 0 var(--color-box-shadow);
+}
+
+.content-header-title {
+  font-weight: bold;
+  font-size: calc(var(--default-font-size) * 1.5);
+  text-transform: capitalize;
+}
+</style>
---
+++ pages/tools/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <Tools>
+    <div v-for="tool in tools" :key="tool.code" class="tool-card"/>
+  </Tools>
+</template>
+
+<script setup lang="ts">
+import Tools from '~/layouts/Tools.vue';
+
+definePageMeta({
+  auth: false,
+})
+
+const tools = ref([]);
+
+onMounted(async () => {
+  try {
+    const { data } = await $fetch('/api/tools');
+    if (data.value) {
+      tools.value = data.value;
+    }
+  } catch (error) {
+    console.error('Error fetching tools:', error);
+    tools.value = [];
+  }
+});
+</script>
+
+
+<style scoped lang="scss"/>
